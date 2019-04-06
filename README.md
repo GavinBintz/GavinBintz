32dec6a29925355277f06604da267d018ef3f8b4
unknown <gavinbintz>
2019-04-05 21:30:52 -0500

    Dark Theme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gavinbintz
+# Gavin Bintz's Personal Website
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gavinbintz';
+  title = 'Gavin Bintz\'s personal website';
 }
